c3d1b2c5bab44ff3b61898402dfce2d3c06f2d9c
Rahman <rahman.n1234567@gmail.com>
2023-02-14 01:00:13 -0800

    update ide section

---
+++ src/app/codefaceApi.js
@@ -0,0 +1,13 @@
+import{createApi,fetchBaseQuery }from  '@reduxjs/toolkit/query/react'
+
+
+
+export  const  codefaceApi  =  createApi({
+  reducerPath:  'codefaceApi' ,
+  baseQuery: fetchBaseQuery({ baseUrl:  'https://jsonplaceholder.typicode.com' }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () =>  '/posts' ,
+    }),
+  }),
+})
